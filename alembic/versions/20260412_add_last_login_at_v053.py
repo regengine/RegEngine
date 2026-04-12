07e49c5c32abbb82df80d0396634c5ed1d339de5
Christopher <131207452+PetrefiedThunder@users.noreply.github.com>
2026-04-12 16:39:37 -0700

    fix: deduplicate v053 migration revision ID, fix v054 chain

--- alembic/versions/20260412_add_last_login_at_v053.py
+++ alembic/versions/20260412_add_last_login_at_v053.py
@@ -1,6 +1,6 @@
 """Add last_login_at column to users table
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: 97a8b9c0d1e2
 Revises: f6a7b8c9d0e1
 Create Date: 2026-04-12 00:00:00.000000
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "a1b2c3d4e5f6"
+revision: str = "97a8b9c0d1e2"
 down_revision: Union[str, None] = "f6a7b8c9d0e1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
